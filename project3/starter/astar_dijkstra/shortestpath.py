9e8d73044effe2509b321ff352eac0d81579751b
Kevin Ying <yingk@berkeley.edu>
2026-02-23 17:43:18 -0800

    shortest path

--- project3/starter/astar_dijkstra/shortestpath.py
+++ project3/starter/astar_dijkstra/shortestpath.py
@@ -1,3 +1,5 @@
+import heapq
+
 import numpy as np
 from utils.map_utils import worldtocell, celltoworld, celltonumber, numbertocell
 
@@ -32,5 +34,72 @@ def shortestpath(map_data, start, goal):
 
     ## Enter code here
 
-    path = np.array([]).reshape(0, 2)
-    return path
+    pq  = []  # Priority queue for Dijkstra's algorithm
+    dist = {node: float('inf') for node in nodenumber}  # Distance from start to each node
+    prev = [-1] * num_nodes  # Previous node in optimal path initialization
+
+    # Cell indice of the start position in the grid / cell frame
+    start_cell = worldtocell(leftbound, xy_res, start)
+    goal_cell = worldtocell(leftbound, xy_res, goal)
+
+    start_node = celltonumber(start_cell, mx)
+    goal_node = celltonumber(goal_cell, mx)
+
+    dist[start_node] = 0
+    pq.append((0, start_node))  # (distance, node)
+    final_cost = 0
+    completed = False
+    while pq:
+        current_dist, current_node = heapq.heappop(pq)  # Get node with smallest distance
+        if current_node == goal_node:
+            final_cost = current_dist
+            completed = True
+            break
+
+        cell = numbertocell([mx, my], current_node)
+        neighbors = [
+            (cell[0] + dx, cell[1] + dy)
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        ]
+        valid_neighbors = []
+        for neighbor in neighbors:
+            if 0 <= neighbor[0] < mx and 0 <= neighbor[1] < my:
+                neighbor_node = celltonumber(neighbor, mx)
+                if blockflag[neighbor_node] == 0:  # Check if the neighbor is not blocked
+                    valid_neighbors.append(neighbor_node)
+        
+        for neighbor_node in valid_neighbors:
+            alt_dist = dist[current_node] + xy_res # Adjacent cost to the neighbor
+            if alt_dist < dist[neighbor_node]:
+                dist[neighbor_node] = alt_dist
+                prev[neighbor_node] = current_node
+                heapq.heappush(pq, (alt_dist, neighbor_node))
+            
+
+        # for node in nodenumber:
+        #     if node == celltonumber(start_cell, mx):
+        #         dist[node] = 0
+        #         pq.append((0, node))  # (distance, node)
+
+    path = []
+    
+    if not completed:
+        return np.array(path)  # Return empty path if no path is found
+    current_node = goal_node
+    while current_node != -1:
+        cell = numbertocell([mx, my], current_node)
+        world_pos = celltoworld(leftbound, xy_res, cell)
+        path.append(world_pos)
+        current_node = prev[current_node]
+    
+    path.reverse()  # Reverse the path to get it from start to goal
+    return np.array(path)
+
+
+
+
+
+    # return final_cost
+
+    # path = np.array([]).reshape(0, 2)
+    # return path
