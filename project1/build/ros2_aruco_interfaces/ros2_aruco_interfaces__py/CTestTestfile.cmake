7696e3c4c82ae640dabd38d545170cbffbc36f76
Aaron Zheng <aaronz@berkeley.edu>
2026-03-06 15:46:38 -0800

    first working quadratic unicycle tracker donne

--- project1/build/ros2_aruco_interfaces/ros2_aruco_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cc/ee106b/sp26/class/ee106b-aac/eeca106b-project/project1/build/ros2_aruco_interfaces/ros2_aruco_interfaces__py
-# Build directory: /home/cc/ee106b/sp26/class/ee106b-aac/eeca106b-project/project1/build/ros2_aruco_interfaces/ros2_aruco_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
